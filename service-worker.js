34c6ed90150450b3df38214af54deb5165474c98
Aj-devi <ashleyjpinkham@cox.net>
2025-12-05 16:17:24 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,5 @@
-
-const CACHE_NAME = 'myfinance-cache-v1';
-const FILES_TO_CACHE = [
+const CACHE = 'pastel-finance-cache-v1';
+const FILES = [
   './',
   './index.html',
   './style.css',
@@ -8,22 +7,16 @@ const FILES_TO_CACHE = [
   './manifest.json'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-  );
+self.addEventListener('install', evt => {
+  evt.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => { if (k !== CACHE_NAME) return caches.delete(k); })))
-  );
+self.addEventListener('activate', evt => {
+  evt.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => { if(k !== CACHE) return caches.delete(k); }))));
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((resp) => resp || fetch(event.request))
-  );
+self.addEventListener('fetch', evt => {
+  evt.respondWith(caches.match(evt.request).then(resp => resp || fetch(evt.request)));
 });
